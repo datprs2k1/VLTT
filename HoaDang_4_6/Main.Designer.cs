93ee64aefe9721c334f7eb2512c006ca8fcdb7be
datprs <datprs@gmail.com>
2023-08-09 22:10:36 +0700

    Add project files.

---
+++ HoaDang_4_6/Main.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace HoaDang
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnDevices = new System.Windows.Forms.Button();
+            this.Device = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Device});
+            this.dataGridView1.Location = new System.Drawing.Point(83, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(633, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(72, 286);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(165, 63);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Bắt đầu";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnList1_Click);
+            // 
+            // btnDevices
+            // 
+            this.btnDevices.Location = new System.Drawing.Point(551, 286);
+            this.btnDevices.Name = "btnDevices";
+            this.btnDevices.Size = new System.Drawing.Size(165, 63);
+            this.btnDevices.TabIndex = 2;
+            this.btnDevices.Text = "Thiết bị";
+            this.btnDevices.UseVisualStyleBackColor = true;
+            this.btnDevices.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Device
+            // 
+            this.Device.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Device.DataPropertyName = "Name";
+            this.Device.HeaderText = "Thiết bị";
+            this.Device.MinimumWidth = 6;
+            this.Device.Name = "Device";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDevices);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Main";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnDevices;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Device;
+    }
+}
---
+++ HoaDang_4_6/Program.cs
@@ -0,0 +1,23 @@
+﻿using HoaDang;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HoaDang_4_6
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+    }
+}
